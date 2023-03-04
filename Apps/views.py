c54459b62065041ad9b0a9867be357819cbb3855
Piyu5h07 <piyushbthaware@gmail.com>
2023-03-04 21:40:15 +0530

    first commit

---
+++ Apps/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+# Model
+from . models import *
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, "Home.html",locals())
+
+def about(request):
+    return render(request, "About.html",locals())
+
+def education(request):
+    allEducation = Education.objects.all()
+    return render(request, "Education.html",locals())
+
+def project(request):
+    allProject = Project.objects.all()
+    return render(request, "Project.html",locals())
+
+def contact(request):
+    return render(request, "Contact.html",locals())
